d6fed5d8c25a3a6480b01648bd84c0b85a6eceaa
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2026-08-04 01:18:18 -0500

    Render PPM image

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
+#include <cstdio>
 #include <print>
 
+constexpr int image_width = 512;
+constexpr int image_height = 512;
+constexpr int p3_max_value = 255;
+
 int main() {
-  std::println("Hello!");
+  std::println("P3");
+  std::println("{} {}", image_width, image_height);
+  std::println("{}", p3_max_value);
+
+  for (int row = 0; row < image_height; row++) {
+    std::print(stderr, "\r\x1b[K{}/{} lines remaining", image_height - row,
+               image_height);
+
+    for (int col = 0; col < image_width; col++) {
+      double redness = double(col) / (image_width - 1),
+             greenness = double(row) / (image_height - 1);
+
+      constexpr auto p3_value_from_intensity =
+          [](const double intensity) noexcept -> int {
+        return int((double(p3_max_value) + 0.999) * intensity);
+      };
+
+      int r = p3_value_from_intensity(redness),
+          g = p3_value_from_intensity(greenness), b = 0;
+
+      std::println("{} {} {}", r, g, b);
+    }
+  }
+
+  std::println(stderr, "\r\x1b[KDone!");
 
   return 0;
 }
